bcbcb2191518e5da82aa69298b59d50ed2f55c7e
kjy <amber9291@naver.com>
2021-02-18 15:51:57 +0900

    차트

---
+++ vue-charts/src/components/BarChart.vue
@@ -0,0 +1,39 @@
+<template>
+    <canvas ref="barChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+
+export default {
+    props:['propsdata'],
+    mounted(){
+        var ctx = this.$refs.barChart;
+        var myChart = new this.$_Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: this.propsdata
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                },
+                onClick: function(){
+                    this.$emit('refresh:chart');
+                }
+            }
+        });
+  },
+  created(){
+
+  }
+}
+</script>
+
+<style>
+
+</style>
